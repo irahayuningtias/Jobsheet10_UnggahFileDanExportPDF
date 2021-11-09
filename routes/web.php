a2687344ab661d83fd34962de5fb08122a4f8929
irahayuningtias <2031710085@student.polinema.ac.id>
2021-11-09 16:18:09 +0700

    Jobsheet10_Praktikum3

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ use App\Http\Controllers\ArticleController;
 });*/
 
 Route::resource('articles', ArticleController::class);
+
+Route::get('/article/cetak_pdf', [ArticleController::class, 'cetak_pdf']);
